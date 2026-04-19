3dcfd4085f22b4a1aa9caa7823707984981c4680
Salah Othman <SalahOthmanSaad@gmail.com>
2026-04-19 16:21:56 +0200

    feat: Enhance app initialization and add Hive for local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:TR/core/theme/app_theme.dart';
+import 'package:TR/features/cart/logic/cubit/cart_cubit.dart';
+import 'package:TR/features/home/logic/category/category_cubit.dart';
+import 'package:TR/features/home/logic/products/products_cubit.dart';
 import 'package:TR/features/home/ui/screen/home_screen.dart';
 import 'package:TR/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  // 1. تأكيد تهيئة كل الـ Widgets قبل أي شيء
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. تهيئة Firebase باستخدام الـ Options الخاصة بمشروعك
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // 3. تهيئة Hive للتخزين المحلي (للسلة بدون Auth)
+  await Hive.initFlutter();
+  await Hive.openBox('cart_box');
   runApp(const MyApp());
 }
 
@@ -16,13 +29,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize:  const Size(360, 813),
+      designSize: const Size(360, 813),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: HomeScreen(),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => CategoryCubit()),
+            BlocProvider(create: (_) => ProductsCubit()),
+            BlocProvider(create: (_) => CartCubit()),
+          ],
+          child: MaterialApp(
+            theme: AppTheme.lightTheme,
+            debugShowCheckedModeBanner: false,
+            home: HomeScreen(),
+          ),
         );
       },
     );
