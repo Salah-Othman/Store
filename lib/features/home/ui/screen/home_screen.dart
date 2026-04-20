435ca5ee3663c4271a3f81341ff4fa6ab9a99e43
Salah Othman <SalahOthmanSaad@gmail.com>
2026-04-20 11:46:32 +0200

    feat: Enhance HomeScreen app bar with Cart and Profile navigation buttons

--- lib/features/home/ui/screen/home_screen.dart
+++ lib/features/home/ui/screen/home_screen.dart
@@ -2,10 +2,12 @@ import 'package:TR/core/theme/app_theme.dart';
 import 'package:TR/core/utils/app_sizes.dart';
 import 'package:TR/core/utils/app_strings.dart';
 import 'package:TR/core/widgets/category_item.dart';
+import 'package:TR/features/cart/ui/screen/cart_screen.dart';
 import 'package:TR/features/home/logic/category/category_cubit.dart';
 import 'package:TR/features/home/logic/products/products_cubit.dart';
 import 'package:TR/features/home/ui/widget/bannar_widget.dart';
 import 'package:TR/features/home/ui/widget/product_grid.dart';
+import 'package:TR/features/profile/ui/screen/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,19 +31,40 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          AppStrings.appName,
-          style: Theme.of(
-            context,
-          ).textTheme.displayLarge!.copyWith(color: AppTheme.tertiaryColor),
+        // leading: Icon(Icons.menu, color: AppTheme.primaryColor, size: 30),
+        title: Padding(
+
+          padding:  EdgeInsets.symmetric(horizontal: 12.w),
+          child: Text(
+            AppStrings.appName,
+            style: Theme.of(
+              context,
+            ).textTheme.displayLarge!.copyWith(color: AppTheme.tertiaryColor),
+          ),
         ),
-        centerTitle: true,
+        // centerTitle: true,
         actions: [
-          IconButton(
-            icon: Icon(Icons.search, color: AppTheme.primaryColor, size: 30),
-            onPressed: () {
-              // Handle search action
-            },
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.w),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.shopping_bag, color: AppTheme.primaryColor, size: 30),
+                  onPressed: () {
+                    // Handle search action
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => CartScreen()));
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.person, color: AppTheme.primaryColor, size: 30),
+                  onPressed: () {
+                    // Handle search action
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileScreen()));
+                  },
+                ),
+              ],
+            ),
+            //ProfileScreen
           ),
         ],
       ),
